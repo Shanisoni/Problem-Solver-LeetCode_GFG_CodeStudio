3f8e178ac5c9429382fcf3ccdf1d9830b864cd0f
Shani Soni <shanisoni7084@gmail.com>
2024-02-05 18:24:15 +0530

    No Changes

---
+++ UT Question/Sorted insert for circular linked list.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+using namespace std;
+/* structure for a Node */
+struct Node
+{
+  int data;
+  struct Node *next;
+  
+  Node(int x){
+      data = x;
+      next = NULL;
+  }
+  
+};
+class Solution
+{
+    public:
+    Node *sortedInsert(Node* head, int data)
+    {
+    
+         Node *newNode = new Node(data);
+
+        if (head == NULL)
+        {
+            newNode->next = newNode;
+            return newNode;
+        }
+
+        Node *current = head;
+
+        // Case 1: Insert at the beginning
+        if (data < head->data)
+        {
+            newNode->next = head;
+            Node *temp = head;
+            while (temp->next != head)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+            return newNode;
+        }
+
+        // Case 2: Insert in the middle or at the end
+        while (current->next != head && data > current->next->data)
+        {
+            current = current->next;
+        }
+
+        newNode->next = current->next;
+        current->next = newNode;
+
+        return head;
+    }
+};
+
+void printList(struct Node *start)
+{
+  struct Node *temp;
+
+  if(start != NULL)
+  {
+    temp = start;
+    do { printf("%d ", temp->data);
+      temp = temp->next;
+    } while(temp != start);
+    printf("\n");
+  }
+}
+int main()
+{
+int t,n,x;
+scanf("%d",&t);
+int arr;
+  while(t--){
+  scanf("%d",&n);
+  int list_size, i;
+  
+  /* start with empty linked list */
+  struct Node *start = NULL;
+  struct Node *temp,*r;
+  
+    if(n>0){
+      scanf("%d",&arr);
+    
+    temp = new Node(arr);
+    start=temp;
+    r=start;
+    }
+    for (i = 0; i<n-1; i++)
+    {
+        scanf("%d",&arr);
+        temp = new Node(arr);
+        r->next=temp;
+        r=r->next;
+    }
+    
+    if(n>0)
+    temp->next=start;
+ 
+    scanf("%d",&x);
+    Solution ob;
+    start = ob.sortedInsert(start,x);
+    printList(start);
+    r=start;
+    while(r!=start->next)
+    {
+
+      temp=start;
+      start=start->next;
+      free(temp);
+    }
+    free(start);
+}
+  return 0;
+}
