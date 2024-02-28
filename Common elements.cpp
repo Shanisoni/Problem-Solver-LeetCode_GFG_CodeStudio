06788c13bd964b8e87b6120ce16bc5cf26a176a1
Shani Soni <shanisoni7084@gmail.com>
2024-02-28 22:30:42 +0530

    Solution

---
+++ Common elements.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+    public:    
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+    
+              vector<int> result;
+    int i = 0, j = 0, k = 0;
+
+    while (i < n1 && j < n2 && k < n3) {
+        if (A[i] == B[j] && B[j] == C[k]) {
+            // Found a common element
+            result.push_back(A[i]);
+            // Move all pointers to the next different element
+            int current = A[i];
+            while (i < n1 && A[i] == current) i++;
+            while (j < n2 && B[j] == current) j++;
+            while (k < n3 && C[k] == current) k++;
+        } else {
+            // Move pointers of arrays with smaller values
+            if (A[i] <= B[j] && A[i] <= C[k]) i++;
+            else if (B[j] <= A[i] && B[j] <= C[k]) j++;
+            else if (C[k] <= A[i] && C[k] <= B[j]) k++;
+        }
+    }
+
+    return result;
+        }
+
+};
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; 
+        cin >> n1 >> n2 >> n3;
+        int A[n1];
+        int B[n2];
+        int C[n3];
+        
+        for (int i = 0; i < n1; i++) cin >> A[i];
+        for (int i = 0; i < n2; i++) cin >> B[i];
+        for (int i = 0; i < n3; i++) cin >> C[i];
+        
+        Solution ob;
+        
+        vector <int> res = ob.commonElements (A, B, C, n1, n2, n3);
+        if (res.size () == 0) 
+            cout << -1;
+        for (int i = 0; i < res.size (); i++) 
+            cout << res[i] << " "; 
+        cout << endl;
+    }
+}
