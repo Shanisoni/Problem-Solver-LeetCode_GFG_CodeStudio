956309c1681ab9f340856a5b9cd6b06c9195d502
Shani Soni <shanisoni7084@gmail.com>
2024-03-02 00:49:08 +0530

    Solution

---
+++ First element to occur k times.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int firstElementKTime(int n, int k, int a[])
+    {
+        unordered_map<int,int> mp;
+        for(int i=0; i<n; i++){
+            mp[a[i]]++;
+            if(mp[a[i]] == k){
+                return a[i];
+            }
+        }
+        return -1;
+    }
+};
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n, k;
+	    cin >> n >> k;
+	    int a[n];
+	    for(int i = 0;i<n;i++){
+	        cin >> a[i];
+	    }
+	    Solution ob;
+	    cout<<ob.firstElementKTime(n, k, a)<<endl;
+	}
+	
+	return 0;
+}
