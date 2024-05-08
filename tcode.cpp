9eb1ab626ea805d987972bbf5e688aa4768a0dde
Shani Soni <shanisoni7084@gmail.com>
2024-05-08 15:41:46 +0530

    Commited

---
+++ tcode.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        vector<int> sortedScore = score;
+        sort(sortedScore.begin(), sortedScore.end(), greater<int>());
+        vector<string> medals = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+        unordered_map<int, string> rankMapping;
+        for (int i = 0; i < sortedScore.size(); i++) {
+            if (i < 3) {
+                rankMapping[sortedScore[i]] = medals[i];
+            } else {
+                rankMapping[sortedScore[i]] = to_string(i + 1);
+            }
+        }
+        vector<string> result;
+        for (int s : score) {
+            result.push_back(rankMapping[s]);
+        }
+        return result;
+    }
+};
