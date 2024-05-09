6842adfd49d830d10f06ee9e361b242885436ef7
Shani Soni <shanisoni7084@gmail.com>
2024-05-09 17:11:18 +0530

    Commited

---
+++ Discover-Game.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool divisorGame(int n) {
+        // code here
+        return !(n&1);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution obj;
+        bool ans = obj.divisorGame(n);
+        if (ans)
+            cout << "True" << endl;
+        else
+            cout << "False" << endl;
+    }
+    return 0;
+}
